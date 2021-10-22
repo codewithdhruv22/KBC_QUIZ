cede5f2d6be6c661e5380ef00891edd3d1860c57
Code With Dhruv <75844991+codewithdhruv22@users.noreply.github.com>
2021-10-22 09:33:24 +0530

    Code Of Video No 12

---
+++ VideoNo12Code/lib/widgets/lifeline_sidebar.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class Lifeline_Drawer extends StatefulWidget {
+  const Lifeline_Drawer({ Key? key }) : super(key: key);
+
+  @override
+  _Lifeline_DrawerState createState() => _Lifeline_DrawerState();
+}
+
+class _Lifeline_DrawerState extends State<Lifeline_Drawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: SafeArea(
+        child: Column(
+          children: [
+            Container( margin: EdgeInsets.symmetric(vertical: 12),  child: Text("LifeLine" , style: TextStyle(fontSize: 20 ,fontWeight : FontWeight.bold,))),
+            Row(
+              mainAxisAlignment : MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  children: [
+                    Card(
+                      elevation: 12,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+                      child: Container(
+                        padding : EdgeInsets.all(12),
+                        decoration: BoxDecoration(shape: BoxShape.circle , color: Colors.purpleAccent),
+                      child : Icon(Icons.people ,size : 32,color : Colors.white)
+                      ),
+                    ),
+                    SizedBox(height:5),
+                    Text("Audience\n Poll" , textAlign : TextAlign.center),
+                  ],
+                ),
+                     Column(
+                  children: [
+                    Card(
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+                      child: Container(
+                        padding : EdgeInsets.all(12),
+                        decoration: BoxDecoration(shape: BoxShape.circle , color: Colors.black54),
+                      child : Icon(Icons.change_circle ,size : 32,color : Colors.white)
+                      ),
+                    ),
+                    SizedBox(height:5),
+                    Text("Joker\n Question" , textAlign : TextAlign.center),
+                  ],
+                ),
+                     Column(
+                  children: [
+                    Card(
+                      elevation: 12,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+                      child: Container(
+                        padding : EdgeInsets.all(12),
+                        decoration: BoxDecoration(shape: BoxShape.circle , color: Colors.purpleAccent),
+                      child : Icon(Icons.two_k_plus_sharp ,size : 32,color : Colors.white)
+                      ),
+                    ),
+                    SizedBox(height:5),
+                    Text("Double\n Dip" , textAlign : TextAlign.center),
+                  ],
+                ),
+                     Column(
+                  children: [
+                    Card(
+                      elevation: 12,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+                      child: Container(
+                        padding : EdgeInsets.all(12),
+                        decoration: BoxDecoration(shape: BoxShape.circle , color: Colors.purpleAccent),
+                      child : Icon(Icons.desktop_mac ,size : 32,color : Colors.white)
+                      ),
+                    ),
+                    SizedBox(height:5),
+                    Text("Ask The\n Expert" , textAlign : TextAlign.center),
+                  ],
+                ),
+                
+              ],
+            ),
+SizedBox(height: 5),
+            Divider(color: Colors.black12 , thickness: 1,)
+          ],
+        ),
+      ),
+      
+    );
+  }
+}
